96da52cb2450392297ba9203b93f5cbeffeafd63
zWX933466 <zhouzhihui6@huawei.com>
2021-05-06 21:06:26 +0800

    我跟

--- app/src/main/java/com/example/composedemo/countdownAnimate/CountdownAnimateAC.kt
+++ app/src/main/java/com/example/composedemo/countdownAnimate/CountdownAnimateAC.kt
@@ -1,18 +1,43 @@
 package com.example.composedemo.countdownAnimate
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.runtime.*
+
+enum class Screen {
+    Input, Countdown
+}
 
 class CountdownAnimateAC : AppCompatActivity() {
     companion object {
         val TAG = CountdownAnimateAC::class.java.simpleName
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("11111")
+            MyApp()
+        }
+    }
+
+    @Composable
+    fun MyApp() {
+        var timeInSec = 0
+        Surface(color = MaterialTheme.colors.background) {
+            var screen by remember { mutableStateOf(Screen.Input) }
+            when (screen) {
+                Screen.Input -> {
+                    Text(TAG + "MyApp: screen")
+                }
+                Screen.Countdown -> {
+                    Text(TAG + "MyApp: Countdown")
+                }
+            }
         }
     }
 }

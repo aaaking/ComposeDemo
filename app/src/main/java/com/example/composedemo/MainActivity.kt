5c13e5de2ca70ee11d6205ac99b8c35ad858421e
zWX933466 <zhouzhihui6@huawei.com>
2021-05-06 17:03:23 +0800

    我跟

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -30,7 +30,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
-import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     lateinit var viewModel: HelloViewModel
